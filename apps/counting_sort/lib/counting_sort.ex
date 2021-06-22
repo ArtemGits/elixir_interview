8f908902dba7f8b448835bd87014cd3b642682f1
Artem <artem.salagaev@sredasolutions.com>
2021-06-22 11:42:08 +0300

    wip

--- apps/counting_sort/lib/counting_sort.ex
+++ apps/counting_sort/lib/counting_sort.ex
@@ -30,16 +30,38 @@ defmodule CountingSort do
         put_elem(acc, i, elem(acc, i) + 1)
       end)
 
-    insert(min, {min,max}, counted, [])
+    insert(min, {min, max}, counted, [])
 
     # Enum.flat_map(min..max, &List.duplicate(&1, elem(counted, &1 - min)))
+  end
+
+  def counting_sort1(data, base) do
+    # TODO refactor this logic
+    {min, max} = Enum.min_max(data)
+    init_counter = Tuple.duplicate(0, max - min + 1)
+
+    counted =
+      Enum.reduce(data, init_counter, fn x, acc ->
+        i = div(x, base)
+        put_elem(acc, rem(i, 10), elem(acc, rem(i, 10)) + 1)
+      end)
+
+    insert(min, {min, max}, counted, [], base)
+    |>IO.inspect()
+  end
+
+  defp insert(i, min_max, counted, sorted, base) do
+    i = rem(div(i, base), 10)
+    count = elem(counted, i)
+    sorted = insert(i, count, sorted)
+    insert(i + 1, min_max, counted, sorted)
   end
 
   defp insert(i, {min, max} = min_max, counted, sorted) when i <= max do
     i = i - min
     count = elem(counted, i)
     sorted = insert(i, count, sorted)
-    insert(i+1, min_max, counted, sorted)
+    insert(i + 1, min_max, counted, sorted)
   end
 
   defp insert(_, _, _, sorted), do: sorted
---
+++ apps/radix_sort/test/radix_sort_test.exs
@@ -0,0 +1,8 @@
+defmodule RadixSortTest do
+  use ExUnit.Case
+  doctest RadixSort
+
+  test "greets the world" do
+    assert RadixSort.hello() == :world
+  end
+end
